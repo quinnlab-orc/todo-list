8a42b4d07157cecd7907d4c1240b27b7c1d160cf
quinnlab-orc <58008049+quinnlab-orc@users.noreply.github.com>
2020-10-22 16:24:38 -0500

    Add files via upload
    Styling added, small edits to code

--- App.js
+++ App.js
@@ -1,151 +1,100 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
+
+   // gets 'myLibraryInLocalStorage' from localStorage, or if emtpy, returns empty array
+  const storedLibrary = JSON.parse(localStorage.getItem('myLibraryInLocalStorage') || '[]')
+  
+  // sets state to equal the local storage
+  const [myLibrary, setMyLibrary] = React.useState(storedLibrary); 
+
+  // sets myLibrary as localStorage when myLibrary is changed at all
+  React.useEffect(() => {
+    localStorage.setItem('myLibraryInLocalStorage', JSON.stringify(myLibrary));
+  }, [myLibrary]);
+
+  // setter function, adds a new book to the library
+  // should be passed to a child component
+  function addBook(newBook) {
+    setMyLibrary([newBook, ...myLibrary])
+  }
+
+  // toggle the 'read' boolean on a book in the library
+  function toggleBookRead(index) {
+    // access the desired book
+    const currentBook = myLibrary[index]
+
+    // toggle the 'read' key
+    currentBook.read = !currentBook.read
+
+    // copy the library so we can safely edit
+    const newLibrary = [...myLibrary]
+    
+    // set the edited book
+    newLibrary[index] = currentBook
+    setMyLibrary(newLibrary)
+  }
+
+  function deleteBook(title) {
+    const filteredArray = [...myLibrary].filter(items => items.title !== title)
+    setMyLibrary(filteredArray)
+  }
+
   return (
     <div className="App">
-      <DisplayProjects />
-    </div>
-  );
-}
-
-const DisplayProjects = () => {
-  const storedProjects = JSON.parse(localStorage.getItem('myProjectsInLocalStorage') || '[]')
-  const [myProject, setMyProject] = React.useState(storedProjects)
-  React.useEffect(() => {
-    localStorage.setItem('myProjectsInLocalStorage', JSON.stringify(myProject));
-  }, [myProject])
-
-  const [selectedProject, setSelectedProject] = React.useState()
-
-  const handleProjects = (title) => {
-    setSelectedProject(title)
-  }
-
-  const deleteProject = (title) => {
-    const filteredArray = [...myProject].filter(item => item.title !== title)
-    setMyProject(filteredArray)
-  }
-
-  const AddProject = () => {
-    const initialProjectState = { title: '' }
-    const [newProject, setNewProject] = React.useState(initialProjectState)
-
-    const handleSubmit = () => {
-      console.log(newProject)
-      setMyProject([...myProject, newProject])
-      setNewProject(initialProjectState)
-    }
-
-    return (
-      <div className="newProject">
-        <input type="text" placeholder="New Project" onChange={(event) => setNewProject({ ...newProject, title: event.target.value })} />
-        <input type="submit" onClick={() => handleSubmit()} />
+      <h1 className="titleText">My Library</h1>
+      <div className="bookForm">
+        <AddBooktoLibrary onAddBook={addBook}/>
       </div>
-    )
-  }
-
-  return (
-    <div>
-      <div className="addProject">
-        <AddProject />
-      </div>
-      <div className="projects">
-        {myProject.map(({ title }, index) => (
-          <div key={title} onClick={() => handleProjects(title)}>
-            <h2>{title} <button onClick={() => deleteProject(title)}>X</button> </h2>
-          </div>
-        ))}
-        <DisplayTodos onDisplay={selectedProject}/>
-      </div>
-    </div>
-  )
-}
-
-//for deleting todos when project is deleted, 
-//pass the deleted project down to DIsplayTodos and use that to delete todos from array
-
-const DisplayTodos = (props) => {
-  const storedTodos = JSON.parse(localStorage.getItem('myTodosInLocalStorage') || '[]')
-  const [myTodo, setMyTodo] = React.useState(storedTodos);
-  React.useEffect(() => {
-    localStorage.setItem('myTodosInLocalStorage', JSON.stringify(myTodo));
-  }, [myTodo])
-
-  const handleToggle = (title) => {
-    console.log("handleToggle")
-    let spreadTodos = [...myTodo]
-    let currentTodo = {}
-
-    for (let i = 0; i < spreadTodos.length; i++) {
-      if (spreadTodos[i].title === title) {
-        currentTodo = spreadTodos[i]
-        currentTodo.complete = !currentTodo.complete
-        spreadTodos[i] = currentTodo
-        setMyTodo(spreadTodos)
-      }
-    }
-  }
-
-  const deleteTodo = (title) => {
-    console.log("deleteTodo: " + title)
-    const filteredArray = [...myTodo].filter(item => item.title !== title);
-    console.log(filteredArray)
-    setMyTodo(filteredArray)
-  }
-
-  const selectedProject = props.onDisplay
-  const filterForProject = () => {
-    const filteredTodos = [...myTodo].filter(item => item.project === selectedProject)
-    return filteredTodos;
-  }
-
-  const AddTodo = () => {
-    const initialTodoState = { title: '', description: '', dueDate: '', priority: '', project: '', complete: false }
-    const [newTodo, setNewTodo] = React.useState(initialTodoState);
-
-    const handleSubmit = () => {
-      setMyTodo([...myTodo, newTodo])
-      setNewTodo(initialTodoState)
-    }
-
-    return (
-      <div className="newTodo">
-        <label>
-          <input className="inputTodo" type="text" placeholder="title" onChange={(event) => setNewTodo({ ...newTodo, title: event.target.value })} />
-          <br></br>
-          <input className="inputTodo" type="text" placeholder="description" onChange={(event) => setNewTodo({ ...newTodo, description: event.target.value })} />
-          <br></br>
-          <input className="inputTodo" type="date" placeholder="dueDate" onChange={(event) => setNewTodo({ ...newTodo, dueDate: event.target.value })} />
-          <br></br>
-          <input className="inputTodo" type="text" placeholder="project" onChange={(event) => setNewTodo({ ...newTodo, project: event.target.value })} />
-          <br></br>
-          <select placeholder="priority" onChange={(event) => setNewTodo({ ...newTodo, priority: event.target.value })}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-          </select> &nbsp;
-          <input type="submit" onClick={() => handleSubmit()} />
-        </label>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <AddTodo />
-      <div className="todos">
-        {filterForProject().map(({ title, description, dueDate, priority, project, complete }, index) => (
+      <div className="books"></div>
+      <h2 className="myBook">My Books</h2>
+      <div className="book">
+        {myLibrary.map(({title, author, pages, read}, index) => (
           <div key={title}>
             <h3>{title}</h3>
-            <p>{description} {dueDate}</p>
-            <input type='checkbox' checked={complete} onChange={() => handleToggle(title)} />
-            <button className="delete" onClick={() => deleteTodo(title)}>X</button>
+            <p>
+              {author} &nbsp; {pages} pages &nbsp; Have read: 
+              <input type='checkbox' checked={read} onChange={() => toggleBookRead(index)} />
+              &nbsp;&nbsp; <button className="delete" onClick={() => deleteBook(title)}>X</button>
+            </p>
           </div>
         ))}
       </div>
     </div>
+  );
+};
+
+// initial new book state
+const initialState = {title: '', author: '', pages: '', read: false}
+
+function AddBooktoLibrary(props) {
+  const [newBook, setNewBook] = React.useState(initialState)
+  
+  // add book to library using prop
+  // clear form state after
+  const handleClick = () => {
+    props.onAddBook(newBook)
+    setNewBook(initialState)
+  }
+
+  return (
+    <div>
+      <h3>Add New Book</h3>
+      <input type='text' placeholder="Title" value={newBook.title} onChange={(event) => setNewBook({...newBook, title: event.target.value})} />
+      <br></br>
+      <input type='text' placeholder="Author" value={newBook.author} onChange={(event) => setNewBook({...newBook, author: event.target.value})} />
+      <br></br>
+      <input type='text' placeholder="Pages" value={newBook.pages} onChange={(event) => setNewBook({...newBook, pages: event.target.value})} />
+      <br></br>
+      Read?<input type='checkbox' checked={newBook.read} onChange={(event) => setNewBook({...newBook, read: event.target.checked})} />
+      <br></br>
+      <button onClick={handleClick}>Add Book</button>
+      
+    </div>
   )
-}
+};
 
 export default App;
